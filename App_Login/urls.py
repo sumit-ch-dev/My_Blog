6984faeb23d70ead785aae277a09a65791929364
symit1729 <symit1729@outlook.com>
2022-12-01 03:51:51 +0600

    Blog Project first commit

---
+++ App_Login/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+app_name = 'App_Login'
+urlpatterns = [
+    path('signup/', views.sign_up, name='sign_up'),
+    path('login/', views.login_user, name='login'),
+    path('logout/', views.logout_user, name='logout'),
+    path('profile/', views.profile, name='profile'),
+    path('change-profile/', views.user_change, name='user_change'),
+    path('password/', views.pass_change, name='pass_change'),
+]
---
+++ App_Login/views.py
@@ -0,0 +1,69 @@
+from django.shortcuts import render
+from django.contrib.auth.forms import AuthenticationForm, PasswordChangeForm
+from django.contrib.auth import login, authenticate, logout
+from django.shortcuts import HttpResponseRedirect
+from django.urls import reverse
+from django.contrib.auth.decorators import login_required
+from App_Login.forms import SignUpForm, UserProfileChange
+
+# Create your views here.
+
+def sign_up(request):
+    form = SignUpForm()
+    registered = False
+    if request.method == 'POST':
+        form = SignUpForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            registered = True
+
+    dict = {'form':form, 'registered':registered}
+    return render(request, 'App_Login/signup.html', context=dict)
+
+
+def login_user(request):
+    form = AuthenticationForm()
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return HttpResponseRedirect(reverse('index'))
+    return render(request, 'App_Login/login.html', context={'form':form})
+
+@login_required
+def logout_user(request):
+    logout(request)
+    return HttpResponseRedirect(reverse('index'))
+
+
+@login_required
+def profile(request):
+    return render(request, 'App_Login/profile.html', context={})
+
+@login_required
+def user_change(request):
+    current_user = request.user
+    form = UserProfileChange(instance=current_user)
+    if request.method == 'POST':
+        form = UserProfileChange(request.POST, instance=current_user)
+        if form.is_valid():
+            form.save()
+            form = UserProfileChange(instance=current_user)
+    return render(request, 'App_Login/change_profile.html', context={'form':form}) 
+
+
+@login_required
+def pass_change(request):
+    current_user = request.user
+    changed = False
+    form = PasswordChangeForm(current_user)
+    if request.method == 'POST':
+        form = PasswordChangeForm(current_user, data=request.POST)
+        if form.is_valid():
+            form.save()
+            changed = True
+    return render(request, 'App_Login/change_pass.html', context={'form':form, 'changed':changed})
