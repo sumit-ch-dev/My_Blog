44b29dfc4c835f7a403aa20bc2c448ef883e16cd
symit1729 <symit1729@outlook.com>
2022-12-02 17:32:52 +0600

    Comment section added

--- App_Blog/models.py
+++ App_Blog/models.py
@@ -10,6 +10,9 @@ class Blog(models.Model):
     publish_date = models.DateTimeField(auto_now_add=True)
     update_date = models.DateTimeField(auto_now=True)
 
+    class Meta:
+        ordering = ('-publish_date',)
+
     def __str__(self):
         return self.blog_title
 
@@ -20,6 +23,9 @@ class Comment(models.Model):
     comment = models.TextField()
     comment_date = models.DateTimeField(auto_now_add=True)
 
+    class Meta:
+        ordering = ('-comment_date',)
+
 
     def __str__(self):
         return self.comment
@@ -28,3 +34,7 @@ class Comment(models.Model):
 class Likes(models.Model):
     blog = models.ForeignKey(Blog, on_delete=models.CASCADE, related_name='liked_blog')
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='liked_user')
+
+    
+    def __str__(self):
+        return self.user + 'likes' + self.blog
--- App_Blog/urls.py
+++ App_Blog/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from . import views
+from App_Blog import views
 
 app_name = 'App_Blog'
 urlpatterns = [
-    path('', views.blog_list, name='blog_list'),
+    path('', views.BlogList.as_view(), name='blog_list'),
+    path('write/', views.CreateBlog.as_view(), name='create_blog'),
+    path('details/<slug:slug>', views.blog_details, name='blog_details'),
 ]
